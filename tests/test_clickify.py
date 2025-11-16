247da4fa7c127b9b9f122b3137dfd4197c06b8e4
sphemakh <sphemakh@gmail.com>
2025-11-16 19:17:52 +0200

    expand boolean policies

--- tests/test_clickify.py
+++ tests/test_clickify.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 import os.path
-import sys
 from typing import List, Optional, Tuple
 
 import click
+from click.testing import CliRunner
+from omegaconf import OmegaConf
 
 from scabha.schema_utils import clickify_parameters
 
@@ -12,7 +13,7 @@ schema_file = os.path.join(os.path.dirname(__file__), "test_clickify.yaml")
 
 @click.command()
 @clickify_parameters(schema_file)
-def func(
+def file_config_app(
     name: str,
     i: int,
     j: Optional[float] = 1,
@@ -32,5 +33,54 @@ def func(
     print(f"output: {output}")
 
 
-if __name__ == "__main__":
-    sys.exit(func())
+def test_file_config():
+    runner = CliRunner()
+    result = runner.invoke(file_config_app, "--j 2 Foo 1".split())
+    assert result.exit_code == 0
+
+
+config = OmegaConf.create(
+    {
+        "inputs": {
+            "flag": dict(info="foo Bar", dtype="bool", policies=dict(is_flag=True)),
+            "explicit-flag": dict(info="foo Bar", dtype="bool", policies=dict(explicit_flag=True)),
+            "yes-no-flag": dict(info="foo Bar", dtype="bool"),
+        },
+        "outputs": {},
+    }
+)
+
+
+@click.command()
+@clickify_parameters(config)
+def boolean_policies_app(**kwargs):
+    assert isinstance(kwargs["flag"], bool)
+
+
+def test_boolean_policies_error_is_flag():
+    runner = CliRunner()
+    result = runner.invoke(boolean_policies_app, "--flag true".split())
+    assert "unexpected extra argument" in result.output
+    assert result.exit_code != 0
+
+    result = runner.invoke(boolean_policies_app, ["--flag"])
+    assert result.exit_code == 0
+
+
+def test_boolean_policies_error_explicit_flag():
+    runner = CliRunner()
+    result = runner.invoke(boolean_policies_app, "--explicit-flag FooBar".split())
+    assert "Invalid value" in result.output
+    assert result.exit_code != 0
+
+    result = runner.invoke(boolean_policies_app, "--explicit-flag false".split())
+    assert result.exit_code == 0
+
+
+def test_boolean_policies_explicit_yes_no():
+    runner = CliRunner()
+    result = runner.invoke(boolean_policies_app, "--yes-no-flag".split())
+    assert result.exit_code == 0
+
+    result = runner.invoke(boolean_policies_app, "--no-yes-no-flag".split())
+    assert result.exit_code == 0
